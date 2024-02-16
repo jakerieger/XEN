e987b7742b603a9f02118b45f03bb66cb615b589
Jake Rieger <contact.jakerieger@gmail.com>
2024-02-16 00:02:11 -0500

    refactored profiler and debug ui

---
+++ Core/DebugUI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by conta on 2/15/2024.
+//
+
+#include "DebugUI.h"
---
+++ Core/DebugUI.h
@@ -0,0 +1,16 @@
+//
+// Created by conta on 2/15/2024.
+//
+
+#ifndef DEBUGUI_H
+#define DEBUGUI_H
+
+
+
+class DebugUI {
+
+};
+
+
+
+#endif //DEBUGUI_H
